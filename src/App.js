48f3bcc0f628a3c917f7ec1cfc635c1a147bbf5a
prasoon <prasoon@aufaitux.com>
2023-01-18 10:02:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.scss"
+import Main from './main/Main' 
+
+function App() {
+  return (
+    <div>
+     <Main />
+    </div>
+  )
+}
+
+export default App  
